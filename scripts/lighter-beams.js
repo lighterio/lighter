946481e3be61d7c66b4174b23cc1e9fecf212ad0
Sam Eubank <sameubank+zerious@gmail.com>
2014-06-16 14:13:59 -0700

    Added better watching and stringification

--- scripts/lighter-beams.js
+++ scripts/lighter-beams.js
@@ -1,7 +1,5 @@
-setTimeout(function () {
-  Beams()
-    .on('chug:change', function (files) {
-      // TODO: Iterate over files and reload selectively.
-      location.reload();
-    });
-}, 1);
+Beams()
+  .on('chug:change', function (files) {
+    // TODO: Iterate over files and reload selectively.
+    location.reload();
+  });
