ee936e2784a566a5bffa31dad9126fc88ed25a52
Sam Eubank <sameubank+zerious@gmail.com>
2014-06-22 07:48:41 -0700

    Added config for new commands

--- commands/cli.js
+++ commands/cli.js
@@ -4,14 +4,39 @@ var shellify = require('shellify');
 shellify({
   root: require.resolve('lighter').replace(/\/lighter\.js$/, '/'),
   commands: {
+    debug: {
+      note: 'Starts the app as a "debug" environment',
+      options: {
+      }
+    },
     dev: {
-      note: 'Starts the app in development mode',
+      note: 'Starts the app as a "dev" environment',
       options: {
       },
       alias: 'd'
     },
+    test: {
+      note: 'Starts the app as a "test" environment',
+      options: {
+      }
+    },
+    stage: {
+      note: 'Starts the app as a "stage" environment',
+      options: {
+      }
+    },
+    new: {
+      note: 'Starts the app as a "new" environment',
+      options: {
+      }
+    },
+    canary: {
+      note: 'Starts the app as a "canary" environment',
+      options: {
+      }
+    },
     prod: {
-      note: 'Starts the app in production mode',
+      note: 'Starts the app as a "prod" environment',
       options: {
       },
       alias: 'p'
