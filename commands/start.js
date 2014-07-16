d173de823c4a989e663eef580c84b1c79956505e
Sam Eubank <sameubank+zerious@gmail.com>
2014-07-16 16:04:23 -0700

    Log startup errors if they have changed

--- commands/start.js
+++ commands/start.js
@@ -2,25 +2,40 @@ var shellify = require('shellify');
 var spawn = require('child_process').spawn;
 var Duplex = require('stream').Duplex;
 var util = require('util');
-var previousStart = new Date(0);
 
 // TODO: Make these thresholds configurable.
-var restartDelay = 500; // Try to restart in half a second
-var okTime = 2e3; // Call a restart "ok" if it goes 2 seconds without failing.
+var RESTART_TIME = 500; // Try to restart in half a second.
+var OK_TIME = 1e3; // Call a restart "ok" after 2 seconds without failing.
 
 module.exports = function (env) {
+
+  var previousStart = new Date(0);
+  var failureOutput;
+
+  /**
+   * Get a string of numberless ordered lines for deduping logs.
+   */
+  function munge(text) {
+    var lines = text.split('\n');
+    lines.forEach(function (line, index) {
+      lines[index] = lines[index].replace(/\d+/, '');
+    });
+    lines.sort();
+    text = lines.join('\n');
+    return text;
+  }
 
   /**
    * Start a Lighter app.
    */
   function start() {
-
+    var output;
     var now = new Date();
     var elapsed = now - previousStart;
     previousStart = now;
 
     // If it's been a while since we restarted, call this a clean start.
-    var isCleanStart = elapsed > okTime;
+    var isCleanStart = elapsed > OK_TIME;
 
     // Spawn a child process and make it output to stdout.
     var child = spawn(process.execPath, ['app'], {env: {NODE_ENV: env}});
@@ -35,31 +50,39 @@ module.exports = function (env) {
       pipeToStdout();
     }
 
-    // After a fast failure, use a buffer in case we fail again.
+    // After a fast failure, buffer the output in case we fail again.
     else {
-      var isBuffering = true;
-      var data = '\n';
-
+      output = '\n';
       var append = function (chunk) {
-        data += chunk;
+        output += chunk;
       };
+      child.stdout.on('data', append);
+      child.stderr.on('data', append);
 
-      child.on('data', append);
-
-      // When we've started ok, write data to stdout and start piping.
+      // When we've started ok, write output to stdout and start piping.
       var okTimer = setTimeout(function () {
-        process.stdout.write(data);
+        process.stdout.write(output);
         pipeToStdout();
-        child.removeListener('data', append);
-        data = null;
-      }, okTime);
+        child.stdout.removeListener('output', append);
+        child.stderr.removeListener('output', append);
+        output = '';
+      }, OK_TIME);
     }
 
-    // Restart once a second at most.
+    // When a child process dies, try to restart it.
     child.on('close', function () {
-      process.stdout.write('\u001b[31m.\u001b[39m');
+
+      // If we failed differently, log the new output.
+      if (failureOutput && (munge(output) != munge(failureOutput))) {
+        process.stdout.write(output);
+      }
+      // If we failed the same way, just show another red dot.
+      else {
+        process.stdout.write('\u001b[31m.\u001b[39m');
+      }
+      failureOutput = output;
       clearTimeout(okTimer);
-      setTimeout(start, restartDelay);
+      setTimeout(start, RESTART_TIME);
     });
   }
 
