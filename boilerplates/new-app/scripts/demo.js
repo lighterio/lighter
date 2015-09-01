90bbf115b9ac742f781dd9e98e81ad8db822384d
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-01 05:09:56 -0700

    Dependency updates

--- boilerplates/new-app/scripts/demo.js
+++ boilerplates/new-app/scripts/demo.js
@@ -1,15 +1,15 @@
 // @use jymin/jymin.js
 
-Jymin.onReady(function () {
+Jymin.ready(function () {
   var href
   href = window.favicon
   if (href) {
     Jymin.all('link', function (link) {
       var parent
       if (Jymin.contains(link.rel, 'icon')) {
-        parent = Jymin.getParent(link)
-        Jymin.removeElement(link)
-        return Jymin.addElement(parent, 'link?rel=shortcut icon&href=' + href)
+        parent = Jymin.parent(link)
+        Jymin.remove(link)
+        return Jymin.add(parent, 'link?rel=shortcut icon&href=' + href)
       }
     })
   }
